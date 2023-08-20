f4b4fded40277009f0dbd0c05659597eda8eafba
John Connor <johnconnor04@mail.ru>
2023-08-20 19:08:12 +0700

    regex methods

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * <h2>For Firdavs</h2>
+ * @author John Connor
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+    }
+}
